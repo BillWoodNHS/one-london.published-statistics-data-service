518e4ecf47c3456e11c43b09ef63cf485f35448c
Bill Wood <william.wood1@nhs.net>
2026-06-29 22:31:28 +0100

    fix: presentation view snake_case application

--- dbt/macros/presentation/create_presentation_view.sql
+++ dbt/macros/presentation/create_presentation_view.sql
@@ -4,10 +4,12 @@
 
 {% macro default__create_presentation_view(database_name, presentation_schema, view_name, raw_schema, raw_table, period_columns) %}
     {# Generic implementation for DuckDB #}
+    {% set raw_relation = api.Relation.create(database=database_name, schema=raw_schema, identifier=raw_table) %}
+    {% set select_list = one_london_psds.get_screaming_snake_select_list(raw_relation) %}
     {% set sql %}
         create or replace view {{ adapter.quote(presentation_schema) }}.{{ adapter.quote(view_name) }} as
         select
-            *,
+            {{ select_list }},
             current_timestamp as _LOADED_AT
         from {{ adapter.quote(raw_schema) }}.{{ adapter.quote(raw_table) }}
         where _SUBJECT_PERIOD_FROM is not null
--- dbt/macros/presentation/snowflake/create_presentation_view.sql
+++ dbt/macros/presentation/snowflake/create_presentation_view.sql
@@ -1,8 +1,10 @@
 {% macro snowflake__create_presentation_view(database_name, presentation_schema, view_name, raw_schema, raw_table, period_columns) %}
+    {% set raw_relation = api.Relation.create(database=database_name, schema=raw_schema, identifier=raw_table) %}
+    {% set select_list = one_london_psds.get_screaming_snake_select_list(raw_relation) %}
     {% set sql %}
         create or replace view {{ adapter.quote(database_name) }}.{{ adapter.quote(presentation_schema) }}.{{ adapter.quote(view_name) }} as
         select
-            *,
+            {{ select_list }},
             current_timestamp::timestamp_ntz as _LOADED_AT
         from {{ adapter.quote(database_name) }}.{{ adapter.quote(raw_schema) }}.{{ adapter.quote(raw_table) }}
         where _SUBJECT_PERIOD_FROM is not null
